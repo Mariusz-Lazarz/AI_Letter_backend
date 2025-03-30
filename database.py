bb100de51b66aefb239de8ca32cc1b9d71203d6f
Mariusz-Lazarz <mariusz.lazarz97@gmail.com>
2025-03-30 17:12:50 +0200

    removing old db setup

--- database.py
+++ database.py
@@ -4,16 +4,7 @@ from fastapi import Depends
 from typing import Annotated
 from config import URL_DATABASE
 
-TEST_MODE = os.getenv("TEST_MODE", "false").lower() == "true"
-
-if TEST_MODE:
-    db_url = "sqlite:///:memory:?cache=shared"
-    connect_args = {"check_same_thread": False}
-else:
-    db_url = URL_DATABASE
-    connect_args = {}
-
-engine = create_engine(db_url, echo=True, connect_args=connect_args)
+engine = create_engine(URL_DATABASE, echo=True,)
 
 
 def create_db_and_tables():
