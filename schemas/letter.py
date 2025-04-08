9dceba20c248f293ea508a8a5a86c94a03e1b5fc
Mariusz-Lazarz <mariusz.lazarz97@gmail.com>
2025-04-08 16:50:58 +0200

    adding generate letter data schema

---
+++ schemas/letter.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, field_validator
+import uuid
+
+
+class JobDescriptionValidator:
+    @field_validator("job_desc")
+    def validate_job_description(cls, value):
+        if len(value) < 50:
+            raise ValueError("Job description must be at least 50 characters")
+        if len(value) > 3000:
+            raise ValueError("Job description cannot be longer than 3000 characters long")
+        return value
+
+
+class GenerateCoverLetter(BaseModel, JobDescriptionValidator):
+    cv_id: uuid.UUID
+    job_desc: str
