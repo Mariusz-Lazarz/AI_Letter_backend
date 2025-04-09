2b48f1fc13610058aa46ce930b2b5d2598e915c4
Mariusz-Lazarz <mariusz.lazarz97@gmail.com>
2025-04-09 18:05:48 +0200

    adding openai client

---
+++ services/client_openai.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from config import OPENAI_API_KEY
+from prompts import generate_letter_prompt
+
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+def generate_cover_letter(cv, job):
+    response = client.responses.create(
+        model="gpt-4o",
+        instructions=generate_letter_prompt,
+        input=f"<cv> {cv} </cv> <job> {job} </job>"
+    )
+
+    return response.output_text
+   
