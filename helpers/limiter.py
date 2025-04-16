3e84bdd140939bb83ec5b19fc5ab3edafa91581d
Mariusz-Lazarz <mariusz.lazarz97@gmail.com>
2025-04-16 16:46:51 +0200

    fixing url issue

--- helpers/limiter.py
+++ helpers/limiter.py
@@ -1,12 +1,13 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
+from config import REDIS_URL
 
 
 class RateLimiterService:
     def __init__(self):
         """Initialize the rate limiter"""
         self.limiter = Limiter(
-            key_func=get_remote_address, storage_uri="redis://localhost:6379/0", in_memory_fallback_enabled=False
+            key_func=get_remote_address, storage_uri=REDIS_URL, in_memory_fallback_enabled=False
         )
 
     def limit(self, limit_string: str, key_func=None):
